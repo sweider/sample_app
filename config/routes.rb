b0a9eda76b123c457aa2a4c00f650c49eb6fa617
sweider.b <sweider.b@gmail.com>
2013-07-25 13:55:17 +0300

    Static Pages adding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_page/home"
+  get "static_page/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
